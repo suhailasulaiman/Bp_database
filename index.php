7071ffdd9ac228c513acefd08024ddedea82e3b4
suhailasulaiman <suhaila@mfrlab.org>
2015-04-25 13:55:53 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<style type="text/css">
+</style>
+</head>
+</html>
+<?php 
+include 'core/init.php';
+include 'includes/overall/overall_header.php'; 
+?>
+
+<div id="div1">
+	<h2>:: Welcome to Bp Transcriptome Database</h2>
+
+	<h3>Overview</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum accumsan lorem. Aliquam gravida fermentum magna, vitae bibendum dui sagittis vitae. Proin lacinia, urna malesuada pharetra mattis, nisl ex interdum diam, a ultricies elit leo in nunc. Aenean ullamcorper eget ligula et mollis.</p>
+	
+	<p>Duis nec lorem nisl. Suspendisse potenti. Cras fermentum malesuada pharetra. Nunc pharetra posuere vulputate. Mauris efficitur porttitor est, vitae laoreet sapien accumsan nec. Suspendisse at felis fermentum, sodales ante ac, auctor magna. Maecenas congue dapibus tincidunt.</p>
+	
+</div>
+<?php include 'includes/overall/overall_footer.php'; ?>
